7debb55739e3de46ff8ebbb1f68096be784b93b3
cf7 <cfranco7@lion.lmu.edu>
2017-02-06 22:08:06 -0800

    tests are passing, configured travis.yml file

--- test/hello/hello_spec.js
+++ test/hello/hello_spec.js
@@ -2,8 +2,11 @@ var chai = require("chai");
 var expect = chai.expect;
 var apiroutes = require("../../routes/api_routes.js");
 var request = require('supertest');
+var SPY = require('../../server.js');
 
 // testing build testing
+
+
 
 describe("Hello", function() {
     it("tests the Testing", function(done) {
@@ -12,9 +15,17 @@ describe("Hello", function() {
     });
 });
 
-describe("Create Client", function () {
-    it("creates a new client profile", function (done) {
+describe("Routes", function () {
+    it("retrieve the main page", function (done) {
+        var options = {
+            method: "GET",
+            url: '/'
+        };
         // request.get('/').expect(200); // if expect() gets a number, automatically thinks it's a status code
+        SPY.inject(options, function (response) {
+            expect(response.statusCode).to.eql(200);
+        });
+
         done();
     });
 });
