08287a3bf29accf1dc49c14737baa9bc215811e7
cf7 <cfranco7@lion.lmu.edu>
2017-02-07 17:10:49 -0800

    testing builds again again

--- test/hello/hello_spec.js
+++ test/hello/hello_spec.js
@@ -2,7 +2,7 @@ var chai = require("chai");
 var expect = chai.expect;
 var apiroutes = require("../../routes/api_routes.js");
 var request = require('supertest');
-// var SPY = require('../../server.js');
+var SPY = require('../../server.js');
 
 
 describe("Hello", function() {
@@ -14,14 +14,14 @@ describe("Hello", function() {
 
 describe("Routes", function () {
     it("retrieve the main page", function (done) {
-        // var options = {
-        //     method: "GET",
-        //     url: '/'
-        // };
-        // // request.get('/').expect(200); // if expect() gets a number, automatically thinks it's a status code
-        // SPY.inject(options, function (response) {
-        //     // expect(response.statusCode).to.eql(200);
-        // });
+        var options = {
+            method: "GET",
+            url: '/'
+        };
+        // request.get('/').expect(200); // if expect() gets a number, automatically thinks it's a status code
+        SPY.inject(options, function (response) {
+            // expect(response.statusCode).to.eql(200);
+        });
         done();
     });
 });
