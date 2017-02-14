094f469dc8b956ac67f1aac973df0f0a9bfe28f6
cf7 <cfranco7@lion.lmu.edu>
2017-02-14 04:52:32 -0800

    testing build with new script

--- test/hello/hello_spec.js
+++ test/hello/hello_spec.js
@@ -21,10 +21,6 @@ describe("Routes", function () {
         };
         // request.get('/').expect(200); // if expect() gets a number, automatically thinks it's a status code
         SPY.inject(options, function (response) {
-            console.log("***********************************");
-            console.log("inside Hapi inject");
-            console.log(process.env.DATABASE_URL);
-            console.log(process.env.SPY_KEY);
             // expect(response.statusCode).to.eql(200);
             done();
         });
